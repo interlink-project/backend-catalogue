aa9e4eb218059a43f77cc2e7dcbe50cbf5d50e9c
Julen Badiola <badiolajulen@gmail.com>
2022-06-20 13:22:17 +0200

    Without rabbit

--- catalogue/app/messages.py
+++ catalogue/app/messages.py
@@ -1,9 +1,7 @@
 import json
-import os
-from base64 import b64encode
-import aiormq
 import json
 from uuid import UUID
+import requests
 from starlette_context import context
 from contextvars import ContextVar
 
@@ -26,40 +24,14 @@ class UUIDEncoder(json.JSONEncoder):
             return obj.hex
         return json.JSONEncoder.default(self, obj)
         
-exchange_name = os.environ.get("EXCHANGE_NAME")
-rabbitmq_host = os.environ.get("RABBITMQ_HOST")
-rabbitmq_user = os.environ.get("RABBITMQ_USER")
-rabbitmq_password = os.environ.get("RABBITMQ_PASSWORD")
-
-url = "amqp://{}:{}@{}/".format(rabbitmq_user, rabbitmq_password, rabbitmq_host)
-
 async def log(data: dict):
     if is_logging_disabled():
+        print("logging disabled")
         return
 
     if not "user_id" in data:
-        try:
-            data["user_id"] = context.data.get("user", {}).get("sub", None)
-        except Exception as e:
-            return
-
+        data["user_id"] = context.data.get("user", {}).get("sub", "anonymous")
+            
     data["service"] = "catalogue"
-
-    request = b64encode(json.dumps(data,cls=UUIDEncoder).encode())
-    
-    connection = await aiormq.connect(url)
-    channel = await connection.channel()
-
-    await channel.exchange_declare(
-        exchange=exchange_name, exchange_type='direct'
-    )
-
-    await channel.basic_publish(
-        request, 
-        routing_key='logging', 
-        exchange=exchange_name,
-        properties=aiormq.spec.Basic.Properties(
-            delivery_mode=2
-            # Messages marked as 'persistent' that are delivered to 'durable' queues will be logged to disk. Durable queues are recovered in the event of a crash, along with any persistent messages they stored prior to the crash.
-        )
-    )
+    res = requests.post("http://logging/api/v1/log", data=json.dumps(data,cls=UUIDEncoder), timeout=2)
+    print("message sent", res.json())
